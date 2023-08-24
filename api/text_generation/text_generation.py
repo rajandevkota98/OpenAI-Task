078cbe70f32646a561c1eca32482092a9a3e08d1
rajandevkota98 <r.devkota.98@gmail.com>
2023-08-24 16:35:34 +0545

    fastapi part done

--- api/text_generation/text_generation.py
+++ api/text_generation/text_generation.py
@@ -2,16 +2,15 @@ import openai
 from api.utils import read_yaml
 
 class TextGeneration:
-    def __init__(self, prompt: str):
-        self.prompt = prompt
+    def __init__(self):
         self.credentials = read_yaml('/home/rajan/GPT-Work/crediantials/crediatial.yml')
         self.api_key = self.credentials['OPENAI_API_SECRET']
         openai.api_key = self.api_key
     
-    def generate_text(self):
+    def generate_text(self,prompt: str):
         response = openai.Completion.create(
             engine="text-davinci-003",  
-            prompt=self.prompt,
+            prompt=prompt,
             max_tokens=100
         )
         return response.choices[0].text.strip()
--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+from fastapi import FastAPI, Form, HTTPException
+from starlette.responses import RedirectResponse,FileResponse
 from api.text_generation.text_generation import TextGeneration
+from api.constant import APP_HOST,APP_PORT
+from uvicorn import run as app_run
 
-text = TextGeneration("hello")
-if __name__ == '__main__':
-    text1 = text.generate_text()
-    print(text1)
+
+app = FastAPI()
+origins = ["*"]
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
+
+text_gen = TextGeneration()
+
+@app.post("/generate_text/")
+def generate_text(text: str = Form(...)):
+    generated_text = text_gen.generate_text(text)
+    return {"generated_text": generated_text}
+
+        
+if __name__ == "__main__":
+    app_run(app,host=APP_HOST,port=APP_PORT)
